6f4f2a38ca51474a68d2cf5534bd23eb42115e25
Quentin Pongratz <qpongratz@gmail.com>
2022-04-09 11:05:56 -0500

    Remove unneeded post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,15 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_post, only: %i[ destroy ]
+  before_action :authenticate_user!, except: %i[index]
 
   # GET /posts or /posts.json
   def index
     @posts = Post.includes(:user).all.with_rich_text_body
   end
 
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
-
   # GET /posts/new
   def new
     @post = current_user.posts.build
-  end
-
-  # GET /posts/1/edit
-  def edit
   end
 
   # POST /posts or /posts.json
@@ -26,23 +18,10 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /posts/1 or /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, except: %i[show, edit, update]
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
