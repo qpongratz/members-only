b534979763aa3d6d1f8b1d9a4ec0d45471944ec8
Quentin Pongratz <qpongratz@gmail.com>
2022-04-11 15:31:57 -0500

    Fix link font size in sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_08_225459) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -51,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_225459) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
